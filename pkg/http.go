0c59e6e5f49399be6bc362a9d135adeeb3af3c77
Arnaud Mengus <44212923+isontheline@users.noreply.github.com>
2020-09-01 10:46:06 +0200

    Ability to disable following HTTP redirects foreach plugin (#22)
    Co-authored-by: Arnaud MENGUS <isontheline44212923+isontheline@users.noreply.github.com>

--- pkg/http.go
+++ pkg/http.go
@@ -8,7 +8,7 @@ import (
 )
 
 //HTTPGet return http response of http get request
-func HTTPGet(insecure bool, url string) (*http.Response, error) {
+func HTTPGet(insecure bool, url string, followRedirects bool) (*http.Response, error) {
 	tr := &http.Transport{}
 	if insecure {
 		tr.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -17,6 +17,15 @@ func HTTPGet(insecure bool, url string) (*http.Response, error) {
 		Transport: tr,
 		Timeout:   time.Second * 3,
 	}
+
+	// If we don't want to follow HTTP redirects
+	if followRedirects == false {
+		// We tell the HTTP Client to don't follow them
+		netClient.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+	}
+
 	resp, err := netClient.Get(url)
 	if err != nil {
 		log.Println(err)
